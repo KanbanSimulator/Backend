6a45cde77e197d61a9181a0b9c1d413fba5311fa
Dablup <danya_livitin@mail.ru>
2022-01-21 22:41:48 +0300

    Initial commit

---
+++ src/main/java/inno/kanban/KanbanSimulator/KanbanSimulatorApplication.java
@@ -0,0 +1,13 @@
+package inno.kanban.KanbanSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inno/kanban/KanbanSimulator/KanbanSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package inno.kanban.KanbanSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
