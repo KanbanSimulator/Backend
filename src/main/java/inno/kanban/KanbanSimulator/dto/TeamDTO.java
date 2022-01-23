5852506a106ed4bb0458cca62bb365a580dd03a8
Dablup <danya_livitin@mail.ru>
2022-01-23 16:31:52 +0300

    Added userList and taskList in TeamDTO

--- src/main/java/inno/kanban/KanbanSimulator/dto/TeamDTO.java
+++ src/main/java/inno/kanban/KanbanSimulator/dto/TeamDTO.java
@@ -1,8 +1,12 @@
 package inno.kanban.KanbanSimulator.dto;
 
+import inno.kanban.KanbanSimulator.model.Task;
+import inno.kanban.KanbanSimulator.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -10,4 +14,6 @@ import lombok.NoArgsConstructor;
 public class TeamDTO {
     private Long teamId;
     private String teamName;
+    private List<User> userList;
+    private List<Task> taskList;
 }
