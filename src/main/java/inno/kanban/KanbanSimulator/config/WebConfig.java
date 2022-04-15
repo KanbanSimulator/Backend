5c173d1f0f4f03c5e8b2bbc663eef9058d731772
Mikhail <morser1243@gmail.com>
2022-04-15 18:25:01 +0300

    fix cors[2]

--- src/main/java/inno/kanban/KanbanSimulator/config/WebConfig.java
+++ src/main/java/inno/kanban/KanbanSimulator/config/WebConfig.java
@@ -2,10 +2,12 @@ package inno.kanban.KanbanSimulator.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
