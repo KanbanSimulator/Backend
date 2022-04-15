4be9f635fc551a2dd92452e6f87ec40762913eca
Mikhail <morser1243@gmail.com>
2022-04-15 18:40:01 +0300

    fix cors[3]

--- src/main/java/inno/kanban/KanbanSimulator/config/WebConfig.java
+++ src/main/java/inno/kanban/KanbanSimulator/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("**").allowedMethods("*");
+        registry.addMapping("/**").allowedMethods("*");
     }
 
     @Override
